8b370c6cee1b496175dec06df65e0174b2705b1d
HOUSSAM-NAHLY <ennahlyhoussam58@gmail.com>
2025-10-22 16:02:48 +0100

    Update js.js

--- js.js
+++ js.js
@@ -3,21 +3,21 @@
   const cartItems = document.getElementById("cart-items");
   const cartTotal = document.getElementById("cart-total");
 
-  // نلقاو جميع الأزرار لي عندهم class add-to-cart
+ 
   const buttons = document.querySelectorAll(".add-to-cart");
 
   buttons.forEach(button => {
     button.addEventListener("click", () => {
-      const item = button.parentElement; // العنصر ديال المنتوج
-      const name = item.querySelector("h3").textContent; // سميتو
-      const price = parseFloat(item.querySelector(".price").textContent); // الثمن
+      const item = button.parentElement; 
+      const name = item.querySelector("h3").textContent; 
+      const price = parseFloat(item.querySelector(".price").textContent); 
 
-      // نزيدو فاللائحة
+     
       const li = document.createElement("li");
       li.textContent = `${name} - ${price.toFixed(2)} dh`;
       cartItems.appendChild(li);
 
-      // نزيدو للمجموع
+     
       total += price;
       cartTotal.textContent = "Total : dh" + total.toFixed(2);
     });
